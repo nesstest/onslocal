56f4df1d01ce7ff8248b4a6576a852c064f2b2db
Jackie Developer <jackie.tait@ons.gov.uk>
2015-07-16 11:45:06 +0100

    Sprint 2 development

--- src/main/webapp/resources/javascript/map.js
+++ src/main/webapp/resources/javascript/map.js
@@ -1,5 +1,5 @@
-function createMap(result, validpostCode, levelname){
-	var regionText;	
+function createMap(result, validpostCode, levelname, childname){	
+	var regionText,regionDrillText;	
 		
 	if (typeof levelname === 'undefined') {
 		if (typeof validpostCode === 'undefined') {
@@ -28,10 +28,10 @@ function createMap(result, validpostCode, levelname){
 								  '<div style="background-color:white" class="box__inner border box--padded has-icon">'+			                   
 			                      '<div style="color: rgb(243,113,33); font-size: x-large"><strong>' +postcode+'</strong></div>' +
 			                      '<div style="color: black; font-size:medium;">(Output area ' + result.areas[0].OA[0].area + ')<br><br><strong>Part of:</strong></div>' +
-			                      '<div style="font-size: small;"> - ward (<a style="color: light blue"; href="index.html?nav-search=' + validpostCode + '&amp;levelname=WD">'+ result.areas[0].WD[0].area + ' </a>)' +
-		  		             	  '<br> - Local authority (<a style="color: light blue"; href="index.html?nav-search='+ validpostCode + '&amp;levelname=LAD">'+ result.areas[0].LAD[0].area + '</a>)' + 
+			                      '<div style="margin-top:5px;font-size: small;"> - Ward (<a style="color: light blue"; href="index.html?nav-search=' + validpostCode + '&amp;levelname=WD">'+ result.areas[0].WD[0].area + ' </a>)' +
+		  		             	  '<br> - Local Authority (<a style="color: light blue"; href="index.html?nav-search='+ validpostCode + '&amp;levelname=LAD">'+ result.areas[0].LAD[0].area + '</a>)' + 
 			                       regionText +
-			                      '<br> - National (<a style="color: light blue"; href="index.html?nav-search='+ validpostCode + '&amp;levelname=CTRY">'+ result.areas[0].CTRY[0].area + '</a>)</div>' + 
+			                      '<br> - Country (<a style="color: light blue"; href="index.html?nav-search='+ validpostCode + '&amp;levelname=CTRY">'+ result.areas[0].CTRY[0].area + '</a>)</div>' + 
 			                      '</div>' +
 			                      '</article></div>');	
 		}
@@ -39,11 +39,13 @@ function createMap(result, validpostCode, levelname){
 	else {
 		  // if welsh postcode - no GOR
 		  if (validpostCode === "NP18 1AF"){
-			  regionText = '<span style="display:none;"></span>';			  			 
+			  regionText      = '<span style="display:none;"></span>';	
+			  regionDrillText = '<a style="color: light blue"; href="index.html?nav-search=' + validpostCode + '&amp;levelname=LAD&amp;childname=WD"> - Local Authority </a></div>';	
 		  }
 		  else{
-			  regionText = '<br> - Region (<a style="color: light blue"; href="index.html?nav-search='+ validpostCode + '&amp;levelname=GOR">'+ result.areas[0].GOR[0].area + '</a>)' ;		
-		  }
+			  regionText       = '<div style="font-size: small;"> - Region (<a style="color: light blue"; href="index.html?nav-search='+ validpostCode + '&amp;levelname=GOR">'+ result.areas[0].GOR[0].area + '</a>)' ;
+			  regionDrillText  = '<a style="color: light blue"; href="index.html?nav-search=' + validpostCode + '&amp;levelname=GOR&amp;childname=LAD"> - Region </a></div>';
+		  }		  	  
 		  
 		  // display map for level name
 		  if (levelname ==="WD"){
@@ -52,15 +54,20 @@ function createMap(result, validpostCode, levelname){
 						    result.areas[0].WD[0].areaname+":"+ 
 							result.areas[0].WD[0].arealayername+":"+
 				            result.areas[0].WD[0].markerenvelope+":"+
-			                result.areas[0].WD[0].levelname;
-			  // set orange info box details	
+			                result.areas[0].WD[0].levelname+":"+
+			                result.areas[0].WD[0].childarealist;
+			  
+			  // set orange part of box & drill down details
 			  $('#selArea1').append('<div id="innerDIV"> <article class="box box--orange box--orange--separated-left">' +
 					  '<div style="background-color:white" class="box__inner border box--padded has-icon">'+			                   
                       '<div style="color: rgb(243,113,33); font-size: x-large"><strong>' +result.areas[0].WD[0].area+'</strong></div>' +
                       '<div style="color: black; font-size:medium;">(Ward)<br><br><strong>Part of:</strong></div>' +
-                      '<div style="font-size: small;"> - Local authority (<a style="color: light blue"; href="index.html?nav-search=' + validpostCode + '&amp;levelname=LAD">'+ result.areas[0].LAD[0].area + ' </a>)' +
+                      '<div style="font-size: small;"> - Local Authority (<a style="color: light blue"; href="index.html?nav-search=' + validpostCode + '&amp;levelname=LAD">'+ result.areas[0].LAD[0].area + ' </a>)' +
 		              regionText +  
-                      '<br> - National (<a style="color: light blue"; href="index.html?nav-search='+ validpostCode + '&amp;levelname=CTRY">'+ result.areas[0].CTRY[0].area + '</a>)</div>' + 
+                      '<br> - Country (<a style="color: light blue"; href="index.html?nav-search='+ validpostCode + '&amp;levelname=CTRY">'+ result.areas[0].CTRY[0].area + '</a>)</div>' + 
+                      '<div style="color: black; font-size:medium;padding-top:10px;"><strong>Drill down to :</strong></div>' +
+                      '<div style="font-size: small;">' + 
+                      '<a style="color: light blue"; href="index.html?nav-search=' + validpostCode + '&amp;levelname=WD&amp;childname=OA"> - Output area </a></div>' +
                       '</div>' +
                       '</article></div>');	
 		  }
@@ -79,9 +86,12 @@ function createMap(result, validpostCode, levelname){
 			  $('#selArea1').append('<div id="innerDIV"> <article class="box box--orange box--orange--separated-left">' +
 					  '<div style="background-color:white" class="box__inner border box--padded has-icon">'+			                   
                       '<div style="color: rgb(243,113,33); font-size: x-large"><strong>' +result.areas[0].LAD[0].area+'</strong></div>' +
-                      '<div style="color: black; font-size:medium;">(Local authority)<br><br><strong>Part of:</strong></div>' +
+                      '<div style="color: black; font-size:medium;">(Local Authority)<br><br><strong>Part of:</strong></div>' +
                       regionText + 
-                      '<br> - National (<a style="color: light blue"; href="index.html?nav-search='+ validpostCode + '&amp;levelname=CTRY">'+ result.areas[0].CTRY[0].area + '</a>)</div>' + 
+                      '<br><div style="font-size: small;"> - Country (<a style="color: light blue"; href="index.html?nav-search='+ validpostCode + '&amp;levelname=CTRY">'+ result.areas[0].CTRY[0].area + ' </a>)</div>' + 
+                      '<div style="color: black; font-size:medium;padding-top:10px;"><strong>Drill down to :</strong></div>' +
+                      '<div style="font-size: small;">' + 
+                      '<a style="color: light blue"; href="index.html?nav-search=' + validpostCode + '&amp;levelname=LAD&amp;childname=WD"> - Ward </a></div>' +
                       '</div>' +
                       '</article></div>');				
 		  }
@@ -100,7 +110,10 @@ function createMap(result, validpostCode, levelname){
 					  '<div style="background-color:white" class="box__inner border box--padded has-icon">'+			                   
                       '<div style="color: rgb(243,113,33); font-size: x-large"><strong>' +result.areas[0].GOR[0].area+'</strong></div>' +
                       '<div style="color: black; font-size:medium;">(Region)<br><br><strong>Part of:</strong></div>' +
-                      '<div style="font-size: small;"> - National (<a style="color: light blue"; href="index.html?nav-search='+ validpostCode + '&amp;levelname=CTRY">'+ result.areas[0].CTRY[0].area + ' </a>)</div>' +                      
+                      '<div style="font-size: small;"> - Country (<a style="color: light blue"; href="index.html?nav-search='+ validpostCode + '&amp;levelname=CTRY">'+ result.areas[0].CTRY[0].area + ' </a>)</div>' + 
+                      '<div style="color: black; font-size:medium;padding-top:10px;"><strong>Drill down to :</strong></div>' +
+                      '<div style="font-size: small;">' + 
+                      '<a style="color: light blue"; href="index.html?nav-search=' + validpostCode + '&amp;levelname=GOR&amp;childname=LAD"> - Local Authority </a></div>' +
                       '</div>' +
                       '</article></div>');				
 		  }
@@ -118,144 +131,22 @@ function createMap(result, validpostCode, levelname){
 			  $('#selArea1').append('<div id="innerDIV"> <article class="box box--orange box--orange--separated-left">' +
 					  '<div style="background-color:white" class="box__inner border box--padded has-icon">'+			                   
                       '<div style="color: rgb(243,113,33); font-size: x-large"><strong>' +result.areas[0].CTRY[0].area+'</strong></div>' +
-                      '<div style="color: black; font-size:medium;">(National)<br><br></strong></div>' +                   
+                      '<div style="color: black; font-size:medium;">(Country)<br><br></strong></div>' +  
+                      '<div style="color: black; font-size:medium;padding-top:10px;"><strong>Drill down to :</strong></div>' +
+                      '<div style="font-size: small;">' + 
+                      regionDrillText +
                       '</div>' +
                       '</article></div>');				
 		  }	  
 		  
 	}
+	// call highlight map
+	if (typeof childname === 'undefined') {
+		highlightMap(result, validpostCode, levelname);
+	}
+	// call hover map
+	else {
+		 hoverMap(result, validpostCode, levelname, childname);
+	}	
+}	
 	
-	
-    dojoConfig = {
-       locale: "en",
-       parseOnLoad: true	    	     
-     };		
-	
-        var map;
-
-		require([    
-		"esri/map", 
-		"esri/InfoTemplate",
-		"esri/dijit/Scalebar", 
-		"dojo/parser", 
-		"esri/geometry/Extent", 
-		"esri/layers/FeatureLayer",  
-		"esri/symbols/SimpleLineSymbol", 
-		"esri/symbols/SimpleFillSymbol", 
-		"esri/symbols/TextSymbol", 
-		"esri/renderers/SimpleRenderer", 
-		"esri/renderers/UniqueValueRenderer",  
-		"esri/InfoTemplate",         
-		"esri/layers/LabelLayer",  
-		"dojo/_base/Color",
-		"dojo/on",
-		"dojo/dom",
-		"esri/graphic",  
-		"dojo/domReady!",
-		"dijit/layout/BorderContainer", 
-		"dijit/layout/ContentPane"
-		
-		  ], function( 
-		    Map, InfoTemplate, Scalebar, parser, Extent, FeatureLayer, 
-		    SimpleLineSymbol, SimpleFillSymbol, TextSymbol,SimpleRenderer, UniqueValueRenderer, InfoTemplate,   
-		    LabelLayer, Color, on, dom, Graphic
-		  ) 
-		  { 
-		
-			parser.parse(); 
-			detailsArray = details.split(":");
-		
-			var xmin_env      = parseInt(detailsArray[0]);
-			var ymin_env      = parseInt(detailsArray[1]);
-			var xmax_env      = parseInt(detailsArray[2]);
-			var ymax_env      = parseInt(detailsArray[3]);
-			var area          = detailsArray[4];
-			var areacode      = detailsArray[5];
-			var arealayername = detailsArray[6];
-			var xCoord        = detailsArray[7];
-			var yCoord        = detailsArray[8];
-			//$('#selArea').append(areacode);
-			
-			var postcode      = validpostCode;	
-			
-			var diff = xmax_env-xmin_env;				
-			newxmin  = xmin_env - (2 * diff);					
-			var bbox = new esri.geometry.Extent({xmin:newxmin,ymin:ymin_env,xmax:xmax_env,ymax:ymax_env,spatialReference:{wkid:27700}});
-			
-			if (postcode == null || postcode.length == 0) {
-				map = new Map("map", { 
-					extent: bbox,
-					slider:false,
-					showAttribution: false,
-					logo:false
-					});		           
-			}
-			else {
-				map = new Map("map", { 
-					extent: bbox,
-					slider:true,
-					showAttribution: false,
-					logo:false
-					});		
-			}
-			
-			esriConfig.defaults.io.corsEnabledServers.push("http://services.arcgisonline.com");
-			esriConfig.defaults.io.corsEnabledServers.push("https://mapping.statistics.gov.uk");
-			
-			var dynamicMSLayer = new esri.layers.ArcGISDynamicMapServiceLayer("http://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer")      
-			map.addLayer(dynamicMSLayer);         
-			map.setExtent(bbox.expand(1.1)); 
-		
-			if (postcode == null || postcode.length == 0){
-				
-			}
-			else{
-			 map.on("load", function(){		         
-				 var symbol = new esri.symbol.PictureMarkerSymbol({
-			     "angle": 0,
-			     "xoffset": 0,
-			     "yoffset": 12,
-			     "type": "esriPMS",
-			     "url": "resources/images/map-marker-128.png",
-			     "contentType": "image/png",
-			     "width": 24,
-			     "height": 24
-			  });	        
-				 map.graphics.add(new esri.Graphic(new esri.geometry.Point(xCoord, yCoord, new esri.SpatialReference({ wkid: 27700 })),symbol));
-		    });				
-		   }
-								
-		   var defaultSymbol = new SimpleFillSymbol().setStyle(SimpleFillSymbol.STYLE_NULL); 
-		   defaultSymbol.outline.setStyle(SimpleLineSymbol.STYLE_NULL); 
-			 
-					//create renderer 
-			var renderer = new UniqueValueRenderer(defaultSymbol, areacode); 
-			//add symbol for each possible value 
-			renderer.addValue(area, 
-					new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
-			                new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
-			                new Color([229,78,22]),2),new Color([229,78,22, 0.2])));  	  
-			
-			var infoTemplate = new InfoTemplate();
-			infoTemplate.setTitle("Area:" );
-			infoTemplate.setContent("content to go here");
-			var labelField = areacode; 			
-			var featureLayer = new FeatureLayer("https://mapping.statistics.gov.uk/arcgis/rest/services/"+arealayername+"/MapServer/0", { 
-				infoTemplate: infoTemplate,
-				mode: FeatureLayer.SNAPSHOT, 
-				outFields: [labelField]
-			 });    			   
-			
-			featureLayer.setRenderer(renderer); 
-			//featureLayer.infoTemplate.setContent(templateContent);
-			map.addLayer(featureLayer);		
-			
-			map.on("load", function(){ 				
-			   map.disableMapNavigation();
-			   map.disableKeyboardNavigation();
-			   map.disablePan();
-			   map.disableRubberBandZoom();
-			   map.disableScrollWheelZoom();
-		   }); 
-		});		
-	}	   
