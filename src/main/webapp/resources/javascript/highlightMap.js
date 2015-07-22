4196ac871d219ca5daf5bdb5bf9f55ed1cbbd347
Sue Developer <sue.woodward@ons.gov.uk>
2015-07-22 11:33:05 +0100

    Sprint 2 user story 8.

--- src/main/webapp/resources/javascript/highlightMap.js
+++ src/main/webapp/resources/javascript/highlightMap.js
@@ -1,4 +1,4 @@
-function highlightMap(result, validpostCode, levelname){
+function highlightMap(details, validpostCode){
     dojoConfig = {
        locale: "en",
        parseOnLoad: true	    	     
@@ -24,7 +24,13 @@ function highlightMap(result, validpostCode, levelname){
 		"dojo/_base/Color",
 		"dojo/on",
 		"dojo/dom",
-		"esri/graphic",  
+		"esri/graphic",		
+		"esri/lang",
+		"dojo/number", 
+		"dojo/dom-style", 
+        "dijit/TooltipDialog", 
+        "dijit/popup",
+        "dojo/query",
 		"dojo/domReady!",
 		"dijit/layout/BorderContainer", 
 		"dijit/layout/ContentPane"
@@ -32,7 +38,7 @@ function highlightMap(result, validpostCode, levelname){
 		  ], function( 
 		    Map, HomeButton, InfoTemplate, Scalebar, parser, Extent, FeatureLayer, 
 		    SimpleLineSymbol, SimpleFillSymbol, TextSymbol,SimpleRenderer, UniqueValueRenderer, InfoTemplate,   
-		    LabelLayer, Color, on, dom, Graphic
+		    LabelLayer, Color, on, dom, Graphic, esriLang, number, domStyle, TooltipDialog, dijitPopup, query
 		  ) 
 		  { 
 		
@@ -44,11 +50,13 @@ function highlightMap(result, validpostCode, levelname){
 			var xmax_env      = parseInt(detailsArray[2]);
 			var ymax_env      = parseInt(detailsArray[3]);
 			var area          = detailsArray[4];
-			var areacode      = detailsArray[5];
+			var areaname      = detailsArray[5];
 			var arealayername = detailsArray[6];
 			var xCoord        = detailsArray[7];
 			var yCoord        = detailsArray[8];
-			
+			var levelname     = detailsArray[9];
+			var areacode      = detailsArray[10];		
+						
 			var postcode      = validpostCode;	
 			
 			var diff = xmax_env-xmin_env;
@@ -99,32 +107,96 @@ function highlightMap(result, validpostCode, levelname){
 			  });	        
 				 map.graphics.add(new esri.Graphic(new esri.geometry.Point(xCoord, yCoord, new esri.SpatialReference({ wkid: 27700 })),symbol));
 		    });				
-		   }
-								
-		   var defaultSymbol = new SimpleFillSymbol().setStyle(SimpleFillSymbol.STYLE_NULL); 
-		   defaultSymbol.outline.setStyle(SimpleLineSymbol.STYLE_NULL); 
-			 
-			//create renderer 
-			var renderer = new UniqueValueRenderer(defaultSymbol, areacode); 
+		   }			
+		   
+		   var defaultSymbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
+                   new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
+                   new Color([229,78,22]),1),new Color([0,0,0,0]));  
+		   
+		   var highlightSymbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID, 
+			          new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,new Color([229,78,22]), 2), 
+			          new Color([229,78,22,0.1]));
+			
+		    var outcode;
+		    if (levelname === "OA") {
+		    	outcode = areacode;
+		    }
+		    else{
+		    	outcode = areaname;
+		    }
+		    
+		    //create renderer 
+			var renderer = new UniqueValueRenderer(defaultSymbol, outcode);
+			
 			//add symbol for each possible value 
 			renderer.addValue(area, 
-					new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
-			                new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
-			                new Color([229,78,22]),2),new Color([229,78,22, 0.2])));  	  
+			   new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
+			   new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
+			   new Color([229,78,22]),2),new Color([229,78,22, 0.45])));		
 			
-			var infoTemplate = new InfoTemplate();
-			infoTemplate.setTitle("Area:" );
-			infoTemplate.setContent("content to go here");
-			var labelField = areacode; 			
-			var featureLayer = new FeatureLayer("https://mapping.statistics.gov.uk/arcgis/rest/services/"+arealayername+"/MapServer/0", { 
-				infoTemplate: infoTemplate,
+			var featureLayer = new FeatureLayer("https://mapping.statistics.gov.uk/arcgis/rest/services/"+arealayername+"/MapServer/0", { 				
 				mode: FeatureLayer.SNAPSHOT, 
-				outFields: [labelField]
-			 });    			   
+				outFields: [areacode, areaname]
+			});    			   
+			featureLayer.setRenderer(renderer); 		
 			
-			featureLayer.setRenderer(renderer); 
-			//featureLayer.infoTemplate.setContent(templateContent);
-			map.addLayer(featureLayer);		
+			map.addLayer(featureLayer);	
+			
+			map.infoWindow.resize(245,125);
+		    
+		    dialog = new TooltipDialog({
+		       id: "tooltipDialog",
+		       style: "position:  absolute; width: auto; font: normal normal normal 10pt Helvetica;z-index:100;"
+		    });
+		    dialog.startup();		    
+		  
+		 
+		    
+	        //listen for when the onMouseOver event fires on the countiesGraphicsLayer
+	        //when fired, create a new graphic with the geometry from the event.graphic and add it to the maps graphics layer
+		    var t;
+		    featureLayer.on("mouse-over", function(evt){
+		      map.setMapCursor("pointer"); 
+		      
+	          if (levelname === "OA") {
+	        	   t = "<b>${"+ areacode + "}</b>";
+	          }
+	          else {
+	        	   t = "<b>${"+ areaname + "}</b>";
+	          }	 
+	          
+	          
+	          var content = esriLang.substitute(evt.graphic.attributes,t);
+	          var highlightGraphic = new Graphic(evt.graphic.geometry,highlightSymbol);	         
+	          map.graphics.add(highlightGraphic);
+	          
+	          dialog.setContent(content);
+
+	          domStyle.set(dialog.domNode, "opacity", 0.85);
+	         
+	          dijitPopup.open({
+	            popup: dialog, 
+	            x: evt.pageX,
+	            y: evt.pageY
+	          });
+	        });
+	    
+	        function closeDialog() {
+	          map.graphics.clear();
+	          map.setMapCursor("default"); 
+	          dijitPopup.close(dialog);
+	          var symbol = new esri.symbol.PictureMarkerSymbol({
+				 "angle": 0,
+				 "xoffset": 0,
+				 "yoffset": 12,
+				 "type": "esriPMS",
+				 "url": "resources/images/map-marker-128.png",
+				 "contentType": "image/png",
+				 "width": 24,
+				 "height": 24
+			  });	        
+			  map.graphics.add(new esri.Graphic(new esri.geometry.Point(xCoord, yCoord, new esri.SpatialReference({ wkid: 27700 })),symbol));			   			
+	        } 
 			
 			map.on("load", function(){ 				
 			   map.disableMapNavigation();
@@ -132,6 +204,8 @@ function highlightMap(result, validpostCode, levelname){
 			   map.disablePan();
 			   map.disableRubberBandZoom();
 			   map.disableScrollWheelZoom();
+			   map.graphics.enableMouseEvents();
+			   map.graphics.on("mouse-out", closeDialog);			 
 		   }); 
 		});		
 	}	
