eb8048e59ab9e320df31bc8eb7c33ff64e1cf803
Sue Developer <sue.woodward@ons.gov.uk>
2015-10-19 11:21:55 +0100

    put back undefined in get data function

--- src/main/webapp/resources/javascript/getDataTable.js
+++ src/main/webapp/resources/javascript/getDataTable.js
@@ -1,6 +1,6 @@
 function getData(OA,laCode,laName,parliconCode,parliconName,wardCode,wardName,regionCode,regionName,nationalCode,nationalName,healthName, levelname, areaname, tableType){
 
-	if(regionCode === 'W99999999' || typeof regionCode === 'undefined' || typeof regionCode === null)
+	if(regionCode === 'undefined' || regionCode === 'W99999999' || typeof regionCode === 'undefined' || typeof regionCode === null)
 	{
 		regionCode = nationalCode; 
     }
