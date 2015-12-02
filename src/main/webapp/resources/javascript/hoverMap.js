effa0149d43e81dbb7fb0dd3482f81e1d3eed584
Sue Developer <sue.woodward@ons.gov.uk>
2015-12-02 10:30:33 +0000

    Sprint 9 & issues

--- src/main/webapp/resources/javascript/hoverMap.js
+++ src/main/webapp/resources/javascript/hoverMap.js
@@ -391,8 +391,7 @@ function hoverMap(details, postcode, queryExtent){
 			}); //  getJSON(parliConUrl	
 		}  //  executeQueryTask
 
-		function  WD_boxDetail() {
-			alert("in wadrd in hover map");
+		function  WD_boxDetail() {			
 			var urlParams          = '&amp;cn='+ctryName+'&amp;cc='+ctryCode+'&amp;pn='+parliConName+'&amp;pc='+parliConCode+'&amp;hn='+healthName+'&amp;hc='+ healthCode + '&amp;markerenvelope=' + markerEnvelope + '&amp;pcSearch=false';
 			var urlParams1         = '&amp;areaname='+ctryName+'&amp;areacode='+ctryCode+'&amp;pn='+parliConName+'&amp;pc='+parliConCode+'&amp;hn='+healthName+'&amp;hc='+ healthCode + '&amp;markerenvelope=' + markerEnvelope + '&amp;pcSearch=false';
 			
