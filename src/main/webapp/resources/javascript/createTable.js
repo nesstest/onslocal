e7424fa680adfdee002b1b185ddd262b1240abf8
Jackie Developer <jackie.tait@ons.gov.uk>
2016-01-13 16:31:03 +0000

    Sprint 10 - housing table

--- src/main/webapp/resources/javascript/createTable.js
+++ src/main/webapp/resources/javascript/createTable.js
@@ -258,11 +258,12 @@ function createHousing(extcode, levelname){
     $(document).ready(function(){ 
            $.getJSON(URL1, function(result){
                   //Accommodation type – People
-                  all               = result["QS401EW"].value[0]; 
-                  unshared_house    = result["QS401EW"].value[1];
-                  unshared_flat     = result["QS401EW"].value[2];
-                  unshared_caravan  = result["QS401EW"].value[3];
-                  shared            = result["QS401EW"].value[4];
+        	      all        = result["LC2107EW"].value[0];
+   			      christian  = result["LC2107EW"].value[1];
+   			      muslim     = result["LC2107EW"].value[5];
+   			      buddhist   = result["LC2107EW"].value[2];
+   			      sikh       = result["LC2107EW"].value[6];
+   			      other      = result["LC2107EW"].value[7];
                   
                   $('#qs401ew-all').empty();
                   $('#qs401ew-unshared_house').empty();
